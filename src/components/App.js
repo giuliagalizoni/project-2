c003f44574d68c9436d2da30d6eb71b2ee47a2af
Giulia Galizoni <giuliagalizoni@gmail.com>
2022-04-07 00:32:52 +0200

    importei o browser router, o bootstrap, criei os componentes das paginas, importei em app e adicionei as rotas em app

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,22 @@
+import { Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Home from "../pages/Home";
+import Friends from "../pages/Friends";
+import PostDetails from "../pages/PostDetails";
+import NewPost from "../pages/NewPost";
+import Error from "../pages/Error";
 
 function App() {
   return (
-    <div>
+    <div className="container">
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/friends" element={<Friends />}/>
+      <Route path="/post/:id" element={<PostDetails />}/>
+      <Route path="/new-post" element={<NewPost />}/>
+      <Route path="*" element={<Error />}/>
+    </Routes>
     </div>
   );
 }
