a61861f23be9e8989b8d3515319370d2eefdd522
Giulia Galizoni <giuliagalizoni@gmail.com>
2022-04-07 12:29:36 +0200

    list of posts in home done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
+import {Link} from 'react-router-dom'
 
 function Home() {
   const [state, setState] = useState([]);
@@ -14,7 +15,22 @@ function Home() {
       });
   }, []);
 
-  return <div> Home</div>;
+  return (
+  <div className="container mt-5">
+    <div className="row">
+      <div className="col">
+        {state.map((currentObj) => <div className="card" style={{width: "22rem"}}>
+          <img src={currentObj["url image"]} className="card-img-top" alt="..."/>
+          <div className="card-body">
+            <h5 className="card-title">{currentObj.title}</h5>
+            <p className="card-text">{currentObj.place}</p>
+            <Link to="#" className="btn btn-primary">See more</Link>
+          </div>
+        </div>)}
+      </div>
+    </div>
+  
+  </div>)
 }
 
 export default Home;
