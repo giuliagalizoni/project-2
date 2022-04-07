c56fba5c115244291ad7205b3d22d911902217a4
Giulia Galizoni <giuliagalizoni@gmail.com>
2022-04-07 16:51:14 +0200

    home done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 function Home() {
   const [state, setState] = useState([]);
@@ -16,21 +16,29 @@ function Home() {
   }, []);
 
   return (
-  <div className="container mt-5">
-    <div className="row">
-      <div className="col">
-        {state.map((currentObj) => <div className="card" style={{width: "22rem"}}>
-          <img src={currentObj["url image"]} className="card-img-top" alt="..."/>
-          <div className="card-body">
-            <h5 className="card-title">{currentObj.title}</h5>
-            <p className="card-text">{currentObj.place}</p>
-            <Link to="#" className="btn btn-primary">See more</Link>
+    <div className="container mt-5">
+      <div className="row">
+        {state.map((currentObj) => (
+          <div className="col col-4">
+            <div className="card mt-3" style={{ width: "18rem" }}>
+              <img
+                src={currentObj.urlImage}
+                className="card-img-top"
+                alt={currentObj.title}
+              />
+              <div className="card-body">
+                <h5 className="card-title">{currentObj.title}</h5>
+                <p className="card-text">{currentObj.place}</p>
+                <Link to="#" className="btn btn-primary">
+                  See more
+                </Link>
+              </div>
+            </div>
           </div>
-        </div>)}
+        ))}
       </div>
     </div>
-  
-  </div>)
+  );
 }
 
 export default Home;
