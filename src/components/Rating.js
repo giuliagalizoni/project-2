b687249424c9eea27ad82208ca1b5d487a056fde
Giulia Galizoni <giuliagalizoni@gmail.com>
2022-04-12 12:53:14 +0200

    rating stars done

---
+++ src/components/Rating.js
@@ -0,0 +1,7 @@
+function Rating(props) {
+  let grade = Math.round(props.children);
+  let starRating = "★".repeat(grade) + "☆".repeat(5 - grade);
+  return <p className="rating">{starRating}</p>;
+}
+
+export default Rating;
