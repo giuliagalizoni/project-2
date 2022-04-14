7c2fa092dfaf57edc1b35844d0ad29080c5e4fcf
Junior Vilarino <jrvilarino@gmail.com>
2022-04-14 11:36:10 -0300

    like btn funcionando

--- src/components/LikeBtn.js
+++ src/components/LikeBtn.js
@@ -1,11 +1,15 @@
 import axios from "axios";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { Favorite } from "@carbon/icons-react";
 import { FavoriteFilled } from "@carbon/icons-react";
 
 function LikeBtn(props) {
   const [liked, setLiked] = useState(false);
+  // Setar esse state inicial estava fazendo com que o useEffect que escuta 'clicked' executasse mesmo que o usuário não tivesse clicado no botão
   const [clicked, setClicked] = useState(false);
+  // variável pra saber se o componente está renderizando pela primeira vez ou não
+  const didMount = useRef(false);
+
   function handleClick() {
     if (!clicked) {
       setClicked(true);
@@ -13,6 +17,7 @@ function LikeBtn(props) {
       setClicked(false);
     }
   }
+
   useEffect(() => {
     async function fetchPost() {
       try {
@@ -22,13 +27,21 @@ function LikeBtn(props) {
           `https://ironrest.herokuapp.com/Giulia-Junior-Wasthere/${props.id}`,
           { ...clone, like: clicked }
         );
-        console.log(response.data)
         setLiked(clicked);
       } catch (err) {
         console.error(err);
       }
     }
-    fetchPost();
+
+    // Só rodamos a função para atualizar a API se o componente já renderizou pelo menos mais de uma vez
+    if (didMount.current) {
+      fetchPost();
+    }
+
+    // Assim que o componente renderiza pela primeira vez atualizamos pra true
+    if (!didMount.current) {
+      didMount.current = true;
+    }
   }, [clicked]);
 
   useEffect(() => {
@@ -43,3 +56,50 @@ function LikeBtn(props) {
 }
 
 export default LikeBtn;
+
+// =============================================================================================
+// import axios from "axios";
+// import { useState, useEffect } from "react";
+// import { Favorite } from "@carbon/icons-react";
+// import { FavoriteFilled } from "@carbon/icons-react";
+
+// function LikeBtn(props) {
+//   const [liked, setLiked] = useState(false);
+//   const [clicked, setClicked] = useState(false);
+//   function handleClick() {
+//     if (!clicked) {
+//       setClicked(true);
+//     } else {
+//       setClicked(false);
+//     }
+//   }
+//   useEffect(() => {
+//     async function fetchPost() {
+//       try {
+//         const clone = { ...props.state };
+//         delete clone._id;
+//         const response = await axios.put(
+//           `https://ironrest.herokuapp.com/Giulia-Junior-Wasthere/${props.id}`,
+//           { ...clone, like: clicked }
+//         );
+//         console.log(response.data)
+//         setLiked(clicked);
+//       } catch (err) {
+//         console.error(err);
+//       }
+//     }
+//     fetchPost();
+//   }, [clicked]);
+
+//   useEffect(() => {
+//     setLiked(props.state.like);
+//   }, [props.state.like]);
+
+//   return (
+//     <div className="btn btn-info text-white me-1" onClick={handleClick}>
+//       {liked ? <FavoriteFilled /> : <Favorite />}
+//     </div>
+//   );
+// }
+
+// export default LikeBtn;
